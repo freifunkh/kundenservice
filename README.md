21ea075a230b13528347443db2808e84b299ae05
lemoer <git@irrelefant.net>
2016-10-01 15:13:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+freifunkh kundenservice
+=======================
+
+This is a simple bot hanging around in #freifunkh channel in the
+hackint irc network. Be aware that this is hacked quick and dirty.
+
+Installation
+------------
+
+- Install [sopel](https://sopel.chat/) from your favorite package manager.
+
+- Clone this repository as ~/.sopel/modules:
+
+    $ mkdir ~/.sopel/
+    $ git clone https://github.com/freifunkh/kundenservice ~/.sopel/modules
+
+- Use this configuration as testing configuration:
+
+    # ~/.sopel/default.cfg
+    [core]
+    nick = Kundenservice-testing
+    host = irc.hackint.org
+    use_ssl = true
+    verify_ssl = false
+    port = 9999
+    channels = #freifunkh-test
+    enabled = forum
+    owner = <your-irc-name>
+
+- Start sopel:
+
+    $ sopel
+
+Features
+--------
+
+- Notifies about new threads in https://hannover.freifunk.net/forum
+
+Hacking References
+------------------
+
+- [sopel plugin documentation](https://sopel.chat/docs/plugin.html)
---
+++ forum.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import sopel.module
+import sopel.formatting
+import feedparser
+import threading
+import json
+import os
+
+url = "http://hannover.freifunk.net/forum/syndication.php?limit=15"
+save_file = os.path.expanduser("~/.sopel/seen_forum_posts.json")
+channel = "#freifunkh"
+
+seen = []
+
+# we wan't to avoid multipostings, so we lock this mutex
+mutex = threading.Semaphore(1)
+
+try:
+    with open(save_file, "r") as f:
+        seen = json.loads(f.read())
+except FileNotFoundError:
+    print("save_file {} not found.".format(save_file))
+
+
+@sopel.module.interval(60)
+def check_forum_every_one_min(bot):
+    global seen, mutex
+    mutex.acquire()
+    if channel in bot.channels:
+        feed = feedparser.parse(url)
+
+        count = 0
+        new = False
+        for entry in feed.entries:
+            if count >= 5:
+                break
+
+            banner = sopel.formatting.color(
+                    "[Forum]",
+                    fg=sopel.formatting.colors.PINK
+                    )
+            if not entry.link in seen:
+                new = True
+                bot.msg(channel, "{0} Neues Thema: \"{1.title}\" - {1.link}".format(banner, entry))
+                seen += [entry.link]
+                count += 1
+
+        if new:
+            with open(save_file, "w") as f:
+                f.write(json.dumps(seen))
+    else:
+        print("warning: module forum is enabled, but bot is not in channel %s" % channel )
+
+    mutex.release()
