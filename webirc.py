0f9d7e01db056f7ea3a756ba2dbb57872bad6bda
lemoer <git@irrelefant.net>
2016-11-22 22:57:14 +0100

    webirc: improved phrasing

--- webirc.py
+++ webirc.py
@@ -5,12 +5,12 @@ import sopel.formatting
 message = """Hey {0}!
 Wir heißen Sie herzlich Wilkommen in unserem IRC-Chat.
 Wir würden uns freuen, wenn Sie sich einen Spitznamen geben würden.
-Beim Start des WebIRC werden sie aufgefordert einen Namen zu wählen. Das können sie beim nächsten Mal machen.
+Beim Start des WebIRC werden Sie aufgefordert einen Namen zu wählen. Das können Sie beim nächsten Mal machen.
 Vorerst reicht es, wenn Sie unten in die Chatzeile "/n IhrSpitzname" eingeben.
-Noch ein paar Tipps, falls du zum ersten Mal in einem IRC-Chat unterwegs bist:
-1. Bring ein bisschen Zeit mit, wenn du eine Frage stellst. Nicht jeder wird deine Frage sofort lesen.
-2. Stell deine Frage direkt, ohne um Erlaubnis zu fragen, ob du eine Frage stellen darst.
-Und nun viel Spaß beim freufunken! ;)
+Noch ein paar Tipps, falls Sie zum ersten Mal in einem IRC-Chat unterwegs sind:
+1. Bringen Sie ein bisschen Zeit mit, wenn Sie eine Frage stellen. Nicht jeder wird Ihre Frage sofort lesen.
+2. Stellen Sie Ihre Frage direkt, ohne um Erlaubnis zu bitten, ob Sie eine Frage stellen dürfen.
+Und nun viel Spaß beim Freufunken! ;)
 """
 
 @sopel.module.event('JOIN')
