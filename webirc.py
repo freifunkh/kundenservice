d7ddfe3bedda1f41b9f6c14927481652fbce56f5
lemoer <git@irrelefant.net>
2016-11-23 21:59:57 +0100

    webirc: fix command to change nickname is /nick

--- webirc.py
+++ webirc.py
@@ -6,7 +6,7 @@ message = """Hey {0}!
 Wir heißen Sie herzlich Wilkommen in unserem IRC-Chat.
 Wir würden uns freuen, wenn Sie sich einen Spitznamen geben würden.
 Beim Start des WebIRC werden Sie aufgefordert einen Namen zu wählen. Das können Sie beim nächsten Mal machen.
-Vorerst reicht es, wenn Sie unten in die Chatzeile "/n IhrSpitzname" eingeben.
+Vorerst reicht es, wenn Sie unten in die Chatzeile "/nick IhrSpitzname" eingeben.
 Noch ein paar Tipps, falls Sie zum ersten Mal in einem IRC-Chat unterwegs sind:
 1. Bringen Sie ein bisschen Zeit mit, wenn Sie eine Frage stellen. Nicht jeder wird Ihre Frage sofort lesen.
 2. Stellen Sie Ihre Frage direkt, ohne um Erlaubnis zu bitten, ob Sie eine Frage stellen dürfen.
